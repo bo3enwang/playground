4deac1b360b10a3700ded6d272e87f66263c2acc
Bow3n <pioneerwbw@gmail.com>
2018-09-12 18:48:27 +0800

    x

--- algorithms/src/main/java/com/bow3n/learn/algorithms/easy/ArrayAlgorithms.java
+++ algorithms/src/main/java/com/bow3n/learn/algorithms/easy/ArrayAlgorithms.java
@@ -73,7 +73,18 @@ public class ArrayAlgorithms {
         System.out.println(Arrays.toString(nums));
     }
 
-    public static void rotate1(int[] nums, int k) {
+    public static boolean containsDuplicate_a(int[] nums) {
+        if (nums.length == 0) {
+            return false;
+        }
+        Set<Integer> s = new HashSet<>();
+        for (int num : nums) {
+            s.add(num);
+        }
+        return nums.length != s.size();
+    }
+
+    public void rotate1(int[] nums, int k) {
         if (nums.length == 0 || (k %= nums.length) == 0) {
             return;
         }
@@ -95,18 +106,7 @@ public class ArrayAlgorithms {
         }
     }
 
-    public static boolean containsDuplicate_a(int[] nums) {
-        if (nums.length == 0) {
-            return false;
-        }
-        Set<Integer> s = new HashSet<>();
-        for (int num : nums) {
-            s.add(num);
-        }
-        return nums.length != s.size();
-    }
-
-    public static boolean containsDuplicate_b(int[] nums) {
+    public boolean containsDuplicate_b(int[] nums) {
         if (nums.length == 0) {
             return false;
         }
@@ -128,5 +128,52 @@ public class ArrayAlgorithms {
         Assertions.assertTrue(containsDuplicate_b(new int[]{1, 2, 3, 1}));
         Assertions.assertTrue(containsDuplicate_b(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
         Assertions.assertFalse(containsDuplicate_b(new int[]{1, 2, 3, 4}));
+    }
+
+
+    /**
+     * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+     *
+     * @param nums 非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次
+     * @return 只出现了一次的元素。
+     */
+    public int singleNumber(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || i == nums.length - 1) {
+                if (i == 0 && nums[0] != nums[1]) {
+                    return nums[0];
+                }
+                if (i == nums.length - 1 && nums[nums.length - 1] != nums[nums.length - 2]) {
+                    return nums[nums.length - 1];
+                }
+            } else {
+                if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) {
+                    return nums[i];
+                }
+            }
+        }
+        return nums[0];
+    }
+
+    public int singleNumber_a(int[] A) {
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+        int rst = 0;
+        for (int aA : A) {
+            rst ^= aA;
+        }
+        return rst;
+    }
+
+    @Test
+    public void test_singleNumber() {
+        Assertions.assertEquals(1, singleNumber(new int[]{2, 2, 1}));
+        Assertions.assertEquals(4, singleNumber(new int[]{4, 1, 2, 1, 2}));
+        Assertions.assertEquals(4, singleNumber_a(new int[]{4, 1, 2, 1, 2}));
     }
 }
