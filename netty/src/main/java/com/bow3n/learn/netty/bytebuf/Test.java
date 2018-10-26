7c4a02f4f728534129c8a49158768ceffeb6a012
Bow3n <pioneerwbw@gmail.com>
2018-10-26 17:26:19 +0800

    ByteBuf

---
+++ netty/src/main/java/com/bow3n/learn/netty/bytebuf/Test.java
@@ -0,0 +1,34 @@
+package com.bow3n.learn.netty.bytebuf;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author bowen
+ */
+public class Test {
+    public static void main(String[] args) {
+        testSlice();
+        testCopy();
+    }
+
+    public static void testSlice() {
+        Charset utf8 = Charset.forName("UTF-8");
+        ByteBuf buf = Unpooled.copiedBuffer("Netty in Action rocks!", utf8);
+        ByteBuf sliced = buf.slice(0, 15);
+        System.out.println(sliced.toString(utf8));
+        buf.setByte(0, (byte) 'J');
+        assert buf.getByte(0) == sliced.getByte(0);
+    }
+
+    public static void testCopy() {
+        Charset utf8 = Charset.forName("UTF-8");
+        ByteBuf buf = Unpooled.copiedBuffer("Netty in Action rocks!", utf8);
+        ByteBuf copy = buf.copy(0, 15);
+        System.out.println(copy.toString(utf8));
+        buf.setByte(0, (byte) 'J');
+        assert buf.getByte(0) != copy.getByte(0);
+    }
+}
